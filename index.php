a21f43d6cc29fb3d5f1ebf480c5acae2ee2888ca
PauGuiot15 <pguiot15@elpuig.xeill.net>
2020-11-03 16:20:26 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+echo "<form>";
+echo "<input name='afegir'>";
+echo "<select name='clase'>";
+echo "<option value='1'>Alta</option>";
+echo "<option value='2'>Baixa</output>";
+echo "<input type='submit' value='afegir'>";
+echo "</form>";
+
+echo  $_GET['afegir'];
+
+?>
