5b913d7435332e05c725853d95bdfe540e1ee2cf
PauGuiot15 <pguiot15@elpuig.xeill.net>
2020-11-06 17:25:58 +0100

    ...

--- index.php
+++ index.php
@@ -1,15 +1,56 @@
+<style>
+    .alta{
+        background-color:chartreuse;
+        margin: 1em;
+        padding: .5em;
+    }
+    .baixa{
+        background-color: red;
+        margin: 1em;
+        padding: .5em;
+    }
+
+</style>
+
 <?php 
-$db = new mysqli("localhost", "pau", "pau", "Webtasks");
-
-
 echo "<form>";
 echo "<input name='afegir'>";
 echo "<select name='clase'>";
-echo "<option value='1'>Alta</option>";
-echo "<option value='2'>Baixa</output>";
+echo "<option value='alta'>Alta</option>";
+echo "<option value='baixa'>Baixa</option>";
+echo "</select>";
 echo "<input type='submit' value='afegir'>";
 echo "</form>";
 
-echo  $_GET['afegir'];
+
+$db = new mysqli("localhost", "pau", "pau", "Webtasks");
+
+//query tie
+if (!empty($_GET['afegir'])) {
+    $stmt = $db->prepare("INSERT INTO taulaweb VALUES(?,?)");
+    $stmt->bind_param("ss", $_GET['afegir'], $_GET['clase']);
+    $stmt->execute();
+}
+
+//Eliminar
+if (!empty($_GET['eliminar'])) {
+    $stmt = $db->prepare("DELETE FROM taulaweb WHERE descripcio = ?");
+    $stmt->bind_param("s", $_GET['eliminar']);
+    $stmt->execute();
+}
+
+foreach ($db->query("SELECT * FROM taulaweb") as $fila) {
+
+    echo "<li> 
+            <p class='${fila['prioritat']}'>
+                ${fila['descripcio']}
+                <a href='?eliminar=${fila['descripcio']}'>
+                    <img src='img1.jpg'>
+                </a>
+            </p>    
+        </li> ";
+        
+    
+}
 
 ?>
